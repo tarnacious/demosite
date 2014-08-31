d5af8427b697ab1b68daab353f5806309a489d0e
Tarn Barford <tarn@tarnbarford.net>
2014-08-31 20:21:33 +0200

    Use environemnt variables
    I wanted to avoid this, but I guess it is better.

--- demosite/__init__.py
+++ demosite/__init__.py
@@ -2,16 +2,18 @@ from demosite import app
 import logging.config
 import os
 
-# Load build configuration to
-build_config = os.path.join(app.root_path, '/etc/build.cfg')
-app.config.from_pyfile(build_config)
 
 # Load application configuration
-app.config.from_pyfile(app.config["CONFIG"])
+config_path = os.environ.get("DEMOSITE_CONFIG_PATH")
+if config_path is None:
+    config_path = os.path.join(app.root_path, '../etc/config.cfg')
+app.config.from_pyfile(config_path)
 
 # Load logging configuration
-if "LOGGING" in app.config:
-    logging.config.fileConfig(app.config["LOGGING"])
+logging_config = os.environ.get("DEMOSITE_LOGGING_CONFIG_PATH")
+if logging_config is not None:
+    logging.config.fileConfig(logging_config)
+
 
 def development_server():
     app.run(debug=True)
